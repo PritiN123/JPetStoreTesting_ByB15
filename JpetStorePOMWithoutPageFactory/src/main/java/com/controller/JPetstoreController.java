7712ad64f065373d95737312b5d87a00175b319e
Admin <Admin@SES71231275IP10003777>
2025-01-23 17:54:42 +0530

    Add one File

---
+++ JpetStorePOMWithoutPageFactory/src/main/java/com/controller/JPetstoreController.java
@@ -0,0 +1,11 @@
+package com.controller;
+
+import org.apache.log4j.chainsaw.Main;
+
+public class JPetstoreController {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Controller");
+	}
+}
